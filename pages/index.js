a9e3e420b1504cdf06a05dd8fa8907ddad40399c
tyVespA <migliavacca.marco08@gmail.com>
2023-02-24 23:37:21 +0100

    Refractor scss tests to css

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import s from "/styles/Test.module.scss";
 
 export default function Home() {
   return (
@@ -13,7 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <main className={s.test}>test</main>
+      <main>test</main>
     </>
   );
 }
